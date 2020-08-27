d9862bc2528fc7272237edc69334ddf59111ed7d
rai.maciel <rai.maciel@basis.com.br>
2020-08-27 12:23:42 -0300

    Removendo comentarios

--- src/components/Body/index.tsx
+++ src/components/Body/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Container, VersusTitleLeftLetter, VersusTitleRightLetter, BattleDate, DuelBox, DateBox } from './styles';
+import { Container, DuelBox } from './styles';
 
 import Duelist from '../Duelist';
 import CommentBody from '../CommentBody';
@@ -19,19 +19,7 @@ const Body: React.FC = () => {
       <DuelBox>
       <Duelist idInfo="info" title="Últimas palavras:" img={firstHero} nickname="courtesy"
        lastWords={lastWordsLeft}/>
-      {/* <VersusTitleLeftLetter>
-        V
-      </VersusTitleLeftLetter>
-      <VersusTitleRightLetter>
-        S
-      </VersusTitleRightLetter> */}
-      {/* <DateBox>
-      <BattleDate>
-        21/09/2020
-        <br/>
-        <p id="hour">21:00</p>
-      </BattleDate>
-      </DateBox> */}
+
       <Versus/>
       <Duelist idBanner="brother" idInfo="info-brother" title="Últimas palavras:" id="player-name" img={secondHero} nickname="passfail3d"
       lastWords={lastWordsRight}/>
